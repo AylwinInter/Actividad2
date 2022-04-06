43fd48d2dd4ea72b73a2f67e1ae4e7155a32e174
AylwinInter <aylwin.inter@gmail.com>
2022-04-05 20:53:09 -0600

    parcial 2

---
+++ farmacia/lib/constantes/productos.dart
@@ -0,0 +1,188 @@
+const List prod_1 = [
+  "Medicamentos",
+  "Bebidas y suplementos",
+  "Cuidado de la salud",
+  "Cuidado personal",
+  "Alimentos y bebidas",
+  "Bebes y maternidad",
+];
+const List prod_2 = [
+  "Podcasts",
+  "Made For You",
+  "Charts",
+  "New Releases",
+  "Discover",
+  "Concerts",
+];
+const List prods = [
+  {
+    "img": "assets/images/img_3.jpg",
+    "title": "Vitamina A",
+    "description": "Precio",
+    "song_count": "100 songs",
+    "date": "about 19 hr",
+    "song_url": "songs/1.mp3",
+    "songs": [
+      {"title": "Imagination", "duration": "1:21"},
+      {"title": "Home_", "duration": "2:17"},
+      {"title": "Do I Wanna Know?", "duration": "1:31"},
+      {"title": "Whiskey Sour", "duration": "1:42"},
+      {"title": "Decisions", "duration": "1:29"},
+      {"title": "Trees", "duration": "1:51"},
+      {"title": "Earth", "duration": "1:39"},
+    ]
+  },
+  {
+    "img": "assets/images/img_5.jpg",
+    "title": "Vitamina B",
+    "description": "Precio",
+    "song_count": "324 songs",
+    "date": "about 14 hr",
+    "song_url": "songs/2.mp3",
+    "songs": [
+      {"title": "Kaleidoscope", "duration": "2:01"},
+      {"title": "Larks", "duration": "2:54"},
+      {"title": "Homeland", "duration": "2:22"},
+      {"title": "Une Danse", "duration": "3:03"},
+      {"title": "Almonte", "duration": "2:31"},
+      {"title": "Days Like These", "duration": "4:09"},
+      {"title": "In questo momento", "duration": "2:40"},
+    ]
+  },
+  {
+    "img": "assets/images/img_7.jpg",
+    "title": "Vitamina C",
+    "description": "Precio",
+    "song_count": "195 songs",
+    "date": "about 10 hr",
+    "song_url": "songs/1.mp3",
+    "songs": [
+      {"title": "Escaping Time", "duration": "3:20"},
+      {"title": "Just Look at You", "duration": "3:07"},
+      {"title": "Flowing", "duration": "2:11"},
+      {"title": "With Resolve", "duration": "2:09"},
+      {"title": "Infinite Sustain", "duration": "2:29"},
+      {"title": "Ingénue", "duration": "2:38"},
+      {"title": "Hidden Chambers", "duration": "2:49"},
+    ]
+  },
+  {
+    "img": "assets/images/img_4.jpg",
+    "title": "Vitamina D",
+    "description": "Precio",
+    "song_count": "599 songs",
+    "date": "about 21 hr",
+    "song_url": "songs/2.mp3",
+    "songs": [
+      {"title": "Imagination", "duration": "1:21"},
+      {"title": "Home_", "duration": "2:17"},
+      {"title": "Do I Wanna Know?", "duration": "1:31"},
+      {"title": "Whiskey Sour", "duration": "1:42"},
+      {"title": "Decisions", "duration": "1:29"},
+      {"title": "Trees", "duration": "1:51"},
+      {"title": "Earth", "duration": "1:39"},
+    ]
+  },
+  {
+    "img": "assets/images/img_2.jpg",
+    "title": "Vitamina E",
+    "description": "Precio",
+    "song_count": "317 songs",
+    "date": "about 11 hr",
+    "song_url": "songs/1.mp3",
+    "songs": [
+      {"title": "Imagination", "duration": "1:21"},
+      {"title": "Home_", "duration": "2:17"},
+      {"title": "Do I Wanna Know?", "duration": "1:31"},
+      {"title": "Whiskey Sour", "duration": "1:42"},
+      {"title": "Decisions", "duration": "1:29"},
+      {"title": "Trees", "duration": "1:51"},
+      {"title": "Earth", "duration": "1:39"},
+    ]
+  },
+  {
+    "img": "assets/images/img_6.jpg",
+    "title": "Vitamina B12",
+    "description": "Precio",
+    "song_count": "130 songs",
+    "date": "about 7 hr",
+    "song_url": "songs/2.mp3",
+    "songs": [
+      {"title": "Imagination", "duration": "1:21"},
+      {"title": "Home_", "duration": "2:17"},
+      {"title": "Do I Wanna Know?", "duration": "1:31"},
+      {"title": "Whiskey Sour", "duration": "1:42"},
+      {"title": "Decisions", "duration": "1:29"},
+      {"title": "Trees", "duration": "1:51"},
+      {"title": "Earth", "duration": "1:39"},
+    ]
+  },
+  {
+    "img": "assets/images/img_1.jpg",
+    "title": "Vitamina A+",
+    "description": "Precio",
+    "song_count": "50 songs",
+    "date": "about 17 hr",
+    "song_url": "songs/1.mp3",
+    "songs": [
+      {"title": "Kaleidoscope", "duration": "2:01"},
+      {"title": "Larks", "duration": "2:54"},
+      {"title": "Homeland", "duration": "2:22"},
+      {"title": "Une Danse", "duration": "3:03"},
+      {"title": "Almonte", "duration": "2:31"},
+      {"title": "Days Like These", "duration": "4:09"},
+      {"title": "In questo momento", "duration": "2:40"},
+    ]
+  },
+  {
+    "img": "assets/images/img_8.jpg",
+    "title": "Vitamina B+",
+    "description": "Precio",
+    "song_count": "69 songs",
+    "date": "2 hr 14 min",
+    "song_url": "songs/2.mp3",
+    "songs": [
+      {"title": "Escaping Time", "duration": "3:20"},
+      {"title": "Just Look at You", "duration": "3:07"},
+      {"title": "Flowing", "duration": "2:11"},
+      {"title": "With Resolve", "duration": "2:09"},
+      {"title": "Infinite Sustain", "duration": "2:29"},
+      {"title": "Ingénue", "duration": "2:38"},
+      {"title": "Hidden Chambers", "duration": "2:49"},
+    ]
+  },
+  {
+    "img": "assets/images/img_9.jpg",
+    "title": "Vitamina C+",
+    "description": "Precio",
+    "song_count": "60 songs",
+    "date": "3 hr 25 min",
+    "song_url": "songs/1.mp3",
+    "songs": [
+      {"title": "Imagination", "duration": "1:21"},
+      {"title": "Home_", "duration": "2:17"},
+      {"title": "Do I Wanna Know?", "duration": "1:31"},
+      {"title": "Whiskey Sour", "duration": "1:42"},
+      {"title": "Decisions", "duration": "1:29"},
+      {"title": "Trees", "duration": "1:51"},
+      {"title": "Earth", "duration": "1:39"},
+    ]
+  },
+  {
+    "img": "assets/images/img_10.jpg",
+    "title": "Vitamina D+",
+    "description": "Precio",
+    "song_count": "75 songs",
+    "date": "3 hr 56 min",
+    "song_url": "songs/2.mp3",
+    "songs": [
+      {"title": "Imagination", "duration": "1:21"},
+      {"title": "Home_", "duration": "2:17"},
+      {"title": "Do I Wanna Know?", "duration": "1:31"},
+      {"title": "Whiskey Sour", "duration": "1:42"},
+      {"title": "Decisions", "duration": "1:29"},
+      {"title": "Trees", "duration": "1:51"},
+      {"title": "Earth", "duration": "1:39"},
+    ]
+  }
+];
---
+++ farmacia/lib/paginas/Medicos.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Medicos extends StatefulWidget {
+  const Medicos({ Key? key }) : super(key: key);
+
+  @override
+  State<Medicos> createState() => _MedicosState();
+}
+
+class _MedicosState extends State<Medicos> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
